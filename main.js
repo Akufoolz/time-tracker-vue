ea46b929e22fb8eaefccf7be0765e7b771aa9d0d
Julian Groen <julian.groen@wgu.edu>
2018-05-18 15:47:46 -0600

    Updated time fields to HTML5 time inputs.

--- main.js
+++ main.js
@@ -5,26 +5,31 @@ var vm = new Vue({
             {
                 id: 0,
                 type: "Cases",
-                start: "8:00 AM",
-                end: "8:45 AM",
+                start: "08:00:00",
+                end: "08:45:00",
                 hours: ""
             },
             {
                 id: 1,
                 type: "Meeting",
-                start: "8:45 AM",
-                end: "12:00 PM",
+                start: "08:45:00",
+                end: "12:00:00",
                 hours: ""
  
             },
             {
                 id: 2,
                 type: "Training",
-                start: "12:30 PM",
-                end: "4:30 PM",
+                start: "12:30:00",
+                end: "16:30:00",
                 hours: ""
  
             }
         ]
+    },
+    computed: {
+        calcHours: function() {
+
+        }
     }
 });
