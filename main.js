ad8a560bb30e43d8c3c3b74716c771bfd6d029f9
Akufoolz <akufoolz@gmail.com>
2018-05-19 01:43:30 -0600

    time inputs bound to data and update on change.

--- main.js
+++ main.js
@@ -5,31 +5,51 @@ var vm = new Vue({
             {
                 id: 0,
                 type: "Cases",
-                start: "08:00:00",
-                end: "08:45:00",
+                start: "08:00",
+                end: "08:45",
                 hours: ""
             },
             {
                 id: 1,
                 type: "Meeting",
-                start: "08:45:00",
-                end: "12:00:00",
+                start: "08:45",
+                end: "12:00",
                 hours: ""
  
             },
             {
                 id: 2,
                 type: "Training",
-                start: "12:30:00",
-                end: "16:30:00",
+                start: "12:30",
+                end: "16:30",
                 hours: ""
  
             }
-        ]
+        ],
+        dateTest: ""
     },
-    computed: {
+    methods: {
         calcHours: function() {
-
+            let v = this;
+             v.entries.forEach(el => {
+                let dateStart = new Date(`January 01, 1970 ${el.start}:00`);
+                //el.start = dateStart.toLocaleString('en-US', {hour:'numeric', minute:'numeric', hour12: true})
+                let dateEnd = new Date(`January 01, 1970 ${el.end}:00`);
+                //el.end = dateEnd.toLocaleString('en-US', {hour:'numeric', minute:'numeric', hour12: true})
+                let hours = (((dateEnd - dateStart)/1000)/60)/60;
+                v.$set(el, 'hours', hours);
+            });
+        },
+        dateChange: function() {
+            console.log(`Date1: ${this.dateTest}`);
+            
+            console.log(`Date2: ${date}`);
+            
+            console.log(`Date3: ${this.dateTest}`);
+            
         }
+    },
+    beforeMount() {
+        this.calcHours();
     }
 });
