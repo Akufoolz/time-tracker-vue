4b61c37f0551a1906b75e0be4086f19d2697fa02
Akufoolz <akufoolz@gmail.com>
2018-05-20 22:37:01 -0600

    Refactored hour calculation.

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var vm = new Vue({
+
     el: '#app',
+
     data: {
+
         entries: [
+
             {
                 type: "Cases",
                 start: "08:00",
@@ -22,54 +26,86 @@ var vm = new Vue({
                 hours: ""
             }
         ]
+
     },
+
     methods: {
-        calcHours: function () {
-            let v = this;
-            v.entries.forEach(el => {
-                let dateStart = new Date(`January 01, 1970 ${el.start}:00`);
-                //el.start = dateStart.toLocaleString('en-US', {hour:'numeric', minute:'numeric', hour12: true})
-                let dateEnd = new Date(`January 01, 1970 ${el.end}:00`);
-                //el.end = dateEnd.toLocaleString('en-US', {hour:'numeric', minute:'numeric', hour12: true})
-                let hours = parseFloat((((dateEnd - dateStart) / 1000) / 60) / 60).toFixed(2);
-                v.$set(el, 'hours', hours);
-            });
+
+        // calculate hour total on current row
+        rowHours: function (entry) {
+
+            let dateStart = new Date(`January 01, 1970 ${entry.start}:00`);
+            let dateEnd = new Date(`January 01, 1970 ${entry.end}:00`);
+
+            // convert miliseconds to hours and mask to two decimal places
+            let hours = parseFloat((((dateEnd - dateStart) / 1000) / 60) / 60).toFixed(2);
+
+            entry.hours = hours;
+
+            return entry.hours;
+
         },
+
+        // add a new object to the entries array
         addEntry: function (index) {
-            //let newEntry = { type: "", start: "", end: "", hours: "" };
-            let newEntry = {type: "-None-"};
+
+            let newEntry = { type: "-None-" };
+
+            // create a new table row by adding a new entry to entries array.
             this.entries.splice((index + 1), 0, newEntry);
+
         },
+
+        // remove entry from entries array
         delEntry: function (index) {
+
+            // only remove entry if there are more than one
             if (this.entries.length != 1) {
                 this.entries.splice(index, 1);
             } else {
                 alert("You cannot remove the only existing row.");
             }
         }
+
     },
+
     computed: {
+
+        // populate array with existing types from entry objects
         entryTypes: function () {
+
             let v = this;
+
+            // initialize array with default -None- type
             let array = ["-None-"];
+
             v.entries.forEach(el => {
                 if (array.indexOf(el.type) < 0) {
                     array.push(el.type);
                 }
             });
+
             return array;
         },
+
+        // calculate total hours by summing all entry hours
         totalHours: function () {
+
             let v = this;
             let total = 0;
+
             v.entries.forEach(el => {
                 total += parseFloat(el.hours);
             });
+
             return total.toFixed(2);
         }
+
     },
+
     beforeMount() {
-        this.calcHours();
+
         this.setEntryTypes;
+
     }
 });
