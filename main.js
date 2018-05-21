4ee8628e8657d58b1cd237367f39165219c50f5b
Akufoolz <akufoolz@gmail.com>
2018-05-20 18:37:55 -0600

    Implemented add and delete buttons.

--- main.js
+++ main.js
@@ -3,41 +3,51 @@ var vm = new Vue({
     data: {
         entries: [
             {
-                id: 0,
                 type: "Cases",
                 start: "08:00",
                 end: "08:45",
                 hours: ""
             },
             {
-                id: 1,
                 type: "Meeting",
                 start: "08:45",
                 end: "12:00",
                 hours: ""
- 
+
             },
             {
-                id: 2,
                 type: "Training",
                 start: "12:30",
                 end: "16:30",
                 hours: ""
- 
             }
         ],
     },
     methods: {
-        calcHours: function() {
+        calcHours: function () {
             let v = this;
-             v.entries.forEach(el => {
+            v.entries.forEach(el => {
                 let dateStart = new Date(`January 01, 1970 ${el.start}:00`);
                 //el.start = dateStart.toLocaleString('en-US', {hour:'numeric', minute:'numeric', hour12: true})
                 let dateEnd = new Date(`January 01, 1970 ${el.end}:00`);
                 //el.end = dateEnd.toLocaleString('en-US', {hour:'numeric', minute:'numeric', hour12: true})
-                let hours = parseFloat((((dateEnd - dateStart)/1000)/60)/60).toFixed(2);
+                let hours = parseFloat((((dateEnd - dateStart) / 1000) / 60) / 60).toFixed(2);
                 v.$set(el, 'hours', hours);
             });
+        },
+        addEntry: function (index) {
+            let newEntry = { type: "", start: "", end: "", hours: "" };
+            this.entries.splice((index + 1), 0, newEntry);
+            //this.entries.splice((this.id + 1), 0, this.newEntry);
+            //this.$set(this.entries, (this.id + 1), this.newEntry);
+
+        },
+        delEntry: function (index) {
+            if (this.entries.length != 1) {
+                this.entries.splice(index, 1);
+            } else {
+                alert("You cannot remove the only existing row.");
+            }
         }
     },
     beforeMount() {
